351f433b2e7268a7bd202a779de5bfd7d54ad106
Rob Freiburger <rob@robfreiburger.com>
2011-10-04 07:46:58 -0700

    Code somewhat cleaned up for testing.

--- lolocaust/lolocaust.php
+++ lolocaust/lolocaust.php
@@ -1,45 +1,16 @@
-/* 
-
-this is supposed to be just the kick reason selector that then passes off to the lolcaust.py on the EC2 instance
-right now it calls a curl function to pull the server state (i.e. who's logged in to the server) and then parses it
-to give to the invoke_python.php file on the EC2. eventually most of this will be moved to the EC2 .py script
-
-there is a bunch of hard coded, non-portable curl commands specific to our gameserver host. you will have to sort
-this out on your own, but generally if you see "gameserverhost.com" or instance number 427, that's your clue that
-you need to figure out how to pull the current server roster via html vial curl
-
-again, eventually this should be handled by the lolocaust.py, but this is version 1.0, not 2.0
-
-*/
-
 <?php
-
-/*
-------------------------------------------------------------
-
-lolocaust.php - Kick some fuckin' pubbie scum HOORAH
-
-Not sure what __EVENTVALIDATION, ___VIEWSTATE,
-etc. are but they seem to be constants. They are required!
-
-------------------------------------------------------------
-*/
 
 $_GET['lolocaust'] = true;
 require_once("../vip/include/funcs.php");
 
-define("WEBPANEL_USERNAME", "username1"); // username for gameserverhost.com web panel control thing
-define("WEBPANEL_PASSWORD", "password1"); // password for gameserverhost.com web panel control thing
-define("RCON_PASSWORD", "123456"); // RCON password for gameserver
-define("PYTHON_SCRIPT_URL", "http://ec2.amazonaws.com/lolocaust/invoke_python.php"); // location of invoke_python.py. used for amazon EC2
-define("MAX_PLAYERS", 32);
-
-$viewstate_scrape;
+define("SERVER_ADDRESS", "0.0.0.0");
+define("SERVER_PORT", 48888);
+define("RCON_PASSWORD", "123456");
 $players;
 $pubbies;
 $victim;
 
-/* this keeps people from abusing the kick script; will only run once every minute */
+/* code commented as script will perform checks before modifying VIP list
 
 if (!isset($_GET['force']))
 {	
@@ -47,23 +18,12 @@ if (!isset($_GET['force']))
 		die("lolocaust failed: can only run once per minute");
 }
 
-/* 33% chance of using this reason */
-if (rand(1, 3) == 3)
-{
-	$temp = get_vip_list();
-	$cashier = $temp[rand(0, count($temp) - 1)];
-	unset($temp);
-	
-	$kickReason = "reason: faggot | REPORT ADMIN ABUSE | your admin was: " . $cashier;
-}
-/* standard reasons */
-else
-{
-	$kickReasons = get_kickreasons();
+*/
+
+$kickReasons = get_kickreasons();
 		
-	$randMax = count($kickReasons) - 1;
-	$kickReason = $kickReasons[rand(0, $randMax)];
-}
+$randMax = count($kickReasons) - 1;
+$kickReason = $kickReasons[rand(0, $randMax)];
 
 /*
 
@@ -165,29 +125,15 @@ function DecodePacket($data)
     return array($isFromServer, $isResponse, $sequence, $words);
 }
 
-$sock = fsockopen( "tcp://" . $ip, $query_port);
+$sock = fsockopen( "tcp://" . SERVER_ADDRESS, SERVER_PORT);
 if($sock != false)
 {
     socket_set_timeout($sock, 0, 500000);
-    fwrite($sock,EncodeClientRequest("serverInfo")); // OK, serverName, current playercount, max playercount , gamemode, map    
+    fwrite($sock,EncodeClientRequest("login.hashed"));    
     list($isFromServer, $isResponse, $sequence, $words) = DecodePacket(fread($sock, 4096));
-
-    print_r($words);    
     
-    /*
-    //  If its your server you could add 
-    //
-    //  fwrite($sock,EncodeClientRequest("login.plainText PASSWORD"));
-    //  list($isFromServer, $isResponse, $sequence, $words) = DecodePacket(fread($sock, 4096));
-    //
-    //  fwrite($sock,EncodeClientRequest("admin.listPlayers all));
-    //  list($isFromServer, $isResponse, $sequence, $words) = DecodePacket(fread($sock, 4096)); // clantag, player name, squadID, teamID
-    //
-    //  Then $words contains an array of players on the server (I havn't tested this as I don't have a server to test on)
-    */        
+    print_r($words);
     
-    fwrite($sock,EncodeClientRequest("quit"));
-    list($isFromServer, $isResponse, $sequence, $words) =  DecodePacket(fread($sock, 4096));  
     fclose($sock);
 }
 
@@ -196,76 +142,6 @@ if($sock != false)
 End ported RCON
 
 */
-
-
-/*
-------------------------------------------------------------
-Step 0: Login
-------------------------------------------------------------
-*/
-
-$referers[0] = "http://gameserverhost.com/login.aspx";
-$urls[0] = "http://gameserverhost.com/login.aspx";
-
-$fields[0]['ButtonLogin'] = "Login";
-$fields[0]['CheckBoxRememberMe'] = "on";
-$fields[0]['Password'] = urlencode(WEBPANEL_USERNAME);
-$fields[0]['UserName'] = urlencode(WEBPANEL_PASSWORD);
-$fields[0]['__EVENTARGUMENT'] = null;
-$fields[0]['__EVENTTARGET'] = null;
-$fields[0]['__EVENTVALIDATION'] = urlencode("/gameserverhostasdasdasdasdasdasd/asdasd");
-$fields[0]['__LASTFOCUS'] = null;
-$fields[0]['__VIEWSTATE'] = null;
-$fields[0]['___VIEWSTATE'] = urlencode("gameserverhostasdasdasdasdasdasd/asdasd");
-$fields[0]['scrollLeft'] = 0;
-$fields[0]['scrollTop'] = 0;
-
-/*
-------------------------------------------------------------
-Step 1: Navigate to rcon admin page
-and scrape ___VIEWSTATE
-------------------------------------------------------------
-*/
-
-$referers[1] = "http://gameserverhost.com/service_home.aspx?serviceid=427";
-$urls[1] = "http://gameserverhost.com/gameserverwatcher.aspx?serviceid=427";
-
-/*
-------------------------------------------------------------
-Step 2: Get current player list
-------------------------------------------------------------
-*/
-
-$referers[2] = "http://gameserverhost.com/service_home.aspx?serviceid=427";
-$urls[2] = "http://gameserverhost.com/gameserverwatcher.aspx?serviceid=427";
-
-$fields[2]['RCONCommand'] = urlencode("admin.listPlayers all");
-$fields[2]['RCONPassword'] = urlencode(RCON_PASSWORD);
-$fields[2]['SubmitButtonExecute'] = "Execute";
-$fields[2]['__EVENTVALIDATION'] = urlencode("/By8");
-$fields[2]['__VIEWSTATE'] = null;
-$fields[2]['scrollLeft'] = 0;
-$fields[2]['scrollTop'] = 0;
-
-/*
-------------------------------------------------------------
-Step 4: Kick random player
-------------------------------------------------------------
-*/
-
-$referers[3] = "http://gameserverhost.com/service_home.aspx?serviceid=427";
-$urls[3] = "http:///gameserverwatcher.aspx?serviceid=427";
-
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_COOKIEJAR, "/tmp/wwvip-cookie.txt");
-curl_setopt($ch, CURLOPT_COOKIEFILE, "/tmp/wwvip-cookie.txt");
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: application/x-www-form-urlencoded"));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.57 Safari/534.24");
-
-$count = count($urls);
 
 for ($i = 0; $i < $count; $i++)
 {
